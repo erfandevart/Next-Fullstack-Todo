b0e20d60002cb70a0c3893ee0326b26c8120d06a
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2025-05-16 13:59:24 +0330

    add utils

---
+++ utils/auth.js
@@ -0,0 +1,15 @@
+import { compare, hash } from "bcryptjs";
+
+async function hashPassword(password) {
+  const hashedPassword = await hash(password, 12);
+  console.log(hashedPassword);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashPassword) {
+  const isValid = await compare(password, hashPassword);
+  console.log(isValid);
+  return isValid;
+}
+
+export { hashPassword, verifyPassword };
