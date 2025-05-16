33242e90745336b34111e1ca3ea76baa89b096ab
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2025-05-16 13:25:36 +0330

    add created model & add Layout

---
+++ models/User.js
@@ -0,0 +1,26 @@
+import { Model, models, Schema, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  lastName: String,
+
+  todos: [{ title: String, status: String }],
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+});
+
+//اگر داخل مدل یوزر بود که هیچی اگر نبود مدل رو براساس اسکیما بساز
+const User = models.User || Model("User", userSchema);
+
+export default User;
